97f5a6ae1d0c0c9e9c6bee4c80adc2d22799a0b6
mau-me <mauriciosm95@gmail.com>
2023-07-12 21:00:55 -0300

    feat: add button to go back to the top of the page

--- src/pages/jogadores.js
+++ src/pages/jogadores.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../layouts";
 import {
   Typography,
@@ -8,12 +8,39 @@ import {
   ListItemText,
   useMediaQuery,
   useTheme,
+  Button,
+  Box,
 } from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import jogadores from "../../data/jogadoresData";
 
 const Jogadores = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 100) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <Layout>
@@ -23,12 +50,42 @@ const Jogadores = () => {
           {jogadores.map((jogador, index) => (
             <ListItem key={index}>
               <ListItemText
-                primary={jogador.nome}
-                secondary={`Idade: ${jogador.idade} anos - Posição: ${jogador.posicao}`}
+                primary={
+                  <React.Fragment>
+                    <Typography variant="subtitle1">{jogador.nome}</Typography>
+                    <Typography variant="subtitle2">{`Idade: ${jogador.idade} anos`}</Typography>
+                    <Typography variant="subtitle2">{`Posição: ${jogador.posicao}`}</Typography>
+                  </React.Fragment>
+                }
               />
             </ListItem>
           ))}
         </List>
+        {showScrollButton && (
+          <Box
+            sx={{
+              position: "fixed",
+              bottom: "2rem",
+              right: "2rem",
+              zIndex: 9999,
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleScrollToTop}
+              sx={{
+                borderRadius: "50%",
+                width: "56px",
+                height: "56px",
+                minWidth: "auto",
+                minHeight: "auto",
+              }}
+            >
+              <KeyboardArrowUpIcon />
+            </Button>
+          </Box>
+        )}
       </Container>
     </Layout>
   );
