b001b515ee44ff48941db5356512d5cb8d1b9d55
mau-me <mauriciosm95@gmail.com>
2023-07-12 21:01:55 -0300

    feat: uses image carousel component

--- src/pages/galeria.js
+++ src/pages/galeria.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../layouts";
 import { Container, Typography, useMediaQuery, useTheme } from "@mui/material";
+import Carousel from "../components/GalleryCarousel";
 
 const Galeria = () => {
   const theme = useTheme();
@@ -12,7 +13,7 @@ const Galeria = () => {
         <Typography variant={isSmallScreen ? "h3" : "h1"}>
           Galeria de Fotos
         </Typography>
-        {/* Restante do conteúdo */}
+        <Carousel />
       </Container>
     </Layout>
   );
